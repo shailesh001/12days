fd499d1e08daf362f8520953311492c590892e4d
Shailesh Patel <shailesh001@mac.com>
2016-12-22 22:19:43 +0000

    12 days written

--- 21days.py
+++ 21days.py
@@ -1,18 +1,49 @@
-def censor(org_text, match_word):
-    censor_str = "*" * len(match_word)
+first_line = "On the %s day of Christmas my true love sent to me:"
 
-    # Split string into a list of strings
-    text_list = org_text.split(" ")
+days = ["first",
+        "second",
+        "third",
+        "fourth",
+        "fifth",
+        "sixth",
+        "seventh",
+        "eighth",
+        "ninth",
+        "tenth",
+        "eleventh",
+        "twelfth"]
 
-    # Iterate text_list with index
-    for index, word in enumerate(text_list):
-            # if word in list matches match_word then replace with ****...
-            if word == match_word:
-                text_list[index] = censor_str
-
-    # Turn list into string of words separated with space
-    return " ".join(text_list)
 
 
-print(censor("this hack is wack hack", "hack"))
+presents = ["a Partridge in a Pear Tree",
+            "Two Turtle Doves",
+            "Three French Hens",
+            "Four Calling Birds",
+            "Five Golden Rings",
+            "Six Geese a Laying",
+            "Seven Swans a Swimming",
+            "Eight Maids a Milking",
+            "Nine Ladies Dancing",
+            "Ten Lords a Leaping",
+            "Eleven Pipers Piping",
+            "12 Drummers Drumming"]
+
+and_partridge = "and a Partridge in a Pear Tree"
+
+# enumerate gives an index and element reference
+for i, day in enumerate(days):
+    print first_line %(day)
+
+    # Print list in reverse range([start], stop[, step])
+    for a in range(i, 0, -1):
+        print presents[a]
+
+    # Need an 'and' on subsequent verses
+    if (i == 0):
+        print presents[0]
+    else:
+        print and_partridge
+    print "\n"
+
+
 
